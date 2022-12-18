126b9fc116bced54fa4c5278ffa8bae2b45dbe24
IsraelPalomoEspaña <israpalomoe@gmail.com>
2022-12-18 19:15:56 +0100

    feat: :sparkles: firts commit

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name'=>$this->faker->name(),
+            'description'=>$this->faker->text(),
+            'importance'=>rand(1,5),
+            'done'=>$this->faker->boolean(),
+            'colour'=>$this->faker->colorName(),
+        ];
+    }
+}
